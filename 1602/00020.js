e09ef9c628fe096053b94679c293db28355cf861
arkan69 <59425945+arkan69@users.noreply.github.com>
2023-02-17 07:49:08 +0700

    Solved 00020

--- 1602/00020.js
+++ 1602/00020.js
@@ -5,6 +5,29 @@
 
 function angkaPrima(angka) {
   // you can only write your code here!
+  if (angka < 2) {
+    return false;
+  }
+  
+  // Check if the angkaber is 2 or 3
+  if (angka === 2 || angka === 3) {
+    return true;
+  }
+  
+  // Check if the angkaber is divisible by 2 or 3
+  if (angka % 2 === 0 || angka % 3 === 0) {
+    return false;
+  }
+  
+  // Check if the angkaber is divisible by any angkaber up to its square root
+  for (let i = 5; i <= Math.sqrt(angka); i += 6) {
+    if (angka % i === 0 || angka % (i + 2) === 0) {
+      return false;
+    }
+  }
+  
+  // If the angkaber passes all the above tests, it's prime
+  return true;
 }
 
 // TEST CASES
