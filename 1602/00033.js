3b863a600b9e3655fdd49388174a25f75d82f868
arkan69 <59425945+arkan69@users.noreply.github.com>
2023-02-17 08:58:20 +0700

    solved 00033

--- 1602/00033.js
+++ 1602/00033.js
@@ -17,22 +17,64 @@
 
 function changeVocals (str) {
   //code di sini
+  const vowelMap = {
+    a: "b",
+    i: "j",
+    u: "v",
+    e: "f",
+    o: "p",
+    A: "B",
+    I: "J",
+    U: "V",
+    E: "F",
+    O: "P",
+  };
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (vowelMap[str[i]]) {
+      result += vowelMap[str[i]];
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 function reverseWord (str) {
   //code di sini
+  return str.split('').reverse().join('');
 }
 
 function setLowerUpperCase (str) {
   //code di sini
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i].toLowerCase();
+    }
+  }
+  return result;
 }
 
 function removeSpaces (str) {
   //code di sini
+  return str.split(' ').join('');
 }
 
 function passwordGenerator (name) {
   //code di sini
+  if (name.length < 5) {
+    return "Minimal karakter yang diinputkan adalah 5 karakter";
+  }
+
+  let changedVowels = changeVocals(name);
+  let reversed = reverseWord(changedVowels);
+  let lowerUpper = setLowerUpperCase(reversed);
+  let result = removeSpaces(lowerUpper);
+
+  return result;
 }
 
 console.log(passwordGenerator('Sergei Dragunov')); // 'VPNVGBRdJFGRFs'
